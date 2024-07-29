392b9e0bed36b7eef596a9ed8ed6022946c7cb0d
Pjiwm <Pim.Munne@gmail.com>
2024-07-29 21:36:57 +0200

    algorithm removes odd vocab via hiragana check

--- src-tauri/vocab_finder/src/lib.rs
+++ src-tauri/vocab_finder/src/lib.rs
@@ -2,6 +2,8 @@ use std::{char, collections::VecDeque};
 
 use jisho::{lookup, Entry};
 
+const HIRAGANA: &str = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽぁぃぅぇぉゃゅょゎゕゖゔゕゖゝゞゟ";
+
 pub fn word_finder(search_str: String) -> Vec<Entry> {
     let mut input: VecDeque<char> = search_str.chars().collect();
     let mut results: Vec<Entry> = Vec::new();
@@ -9,6 +11,7 @@ pub fn word_finder(search_str: String) -> Vec<Entry> {
     while !input.is_empty() {
         let mut word_buffer = String::new();
         let mut last_result: Option<Entry> = None;
+        let mut last_string = String::new();
 
         // Attempt to build and find words until the input is exhausted
         while let Some(c) = input.pop_front() {
@@ -17,6 +20,7 @@ pub fn word_finder(search_str: String) -> Vec<Entry> {
             if let Some(result) = lookup(&lookup_word).first() {
                 let result = result.to_owned().clone();
                 last_result = Some(result);
+                last_string = lookup_word.clone();
             } else {
                 input.push_front(c);
                 break; // Stop if no more results are found
@@ -24,7 +28,11 @@ pub fn word_finder(search_str: String) -> Vec<Entry> {
         }
 
         if let Some(entry) = last_result {
-            results.push(entry);
+            if !entry.kanji.is_empty()
+                && !last_string.chars().all(|letter| HIRAGANA.contains(letter))
+            {
+                results.push(entry);
+            }
         } else {
             // If no valid word was found, move on to the next character
             input.pop_front();
@@ -37,20 +45,6 @@ pub fn word_finder(search_str: String) -> Vec<Entry> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = lookup("今日は何をしてるか？\n母と買い物をしていただよ。");
-        println!("{:?}", result);
-        let result = lookup("今日は");
-        println!("{:?}", result);
-        let result = lookup("今日");
-        println!("{:?}", result);
-        let result = lookup("今日は何");
-        println!("{:?}", result);
-        let result = lookup("は");
-        println!("{:?}", result);
-    }
 
     #[test]
     fn lookup_works() {
