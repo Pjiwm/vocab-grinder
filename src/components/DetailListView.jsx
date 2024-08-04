dfdeefda5e29603d036c3e6cc666cbe49df0f026
Pjiwm <Pim.Munne@gmail.com>
2024-08-05 00:19:11 +0200

    popup detail list when clicking on list name

---
+++ src/components/DetailListView.jsx
@@ -0,0 +1,34 @@
+const DetailListView = ({ listId }) => {
+  return (
+    <div className="flex justify-center h-screen w-full">
+      <table className="table-auto w-full text-left text-white">
+        <thead className="bg-gray-700">
+          <tr>
+            <th className="px-4 py-2">Word</th>
+            <th className="px-4 py-2">Reading</th>
+            <th className="px-4 py-2">Translation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-gray-800">
+            <td className="border px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td className="border px-4 py-2">Malcolm Lockyer</td>
+            <td className="border px-4 py-2">1961</td>
+          </tr>
+          <tr className="bg-gray-700">
+            <td className="border px-4 py-2">Witchy Woman</td>
+            <td className="border px-4 py-2">The Eagles</td>
+            <td className="border px-4 py-2">1972</td>
+          </tr>
+          <tr className="bg-gray-800">
+            <td className="border px-4 py-2">Shining Star</td>
+            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
+            <td className="border px-4 py-2">1975</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DetailListView;
--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -1,9 +1,13 @@
 import { MdOutlineDeleteForever } from 'react-icons/md';
 import { invoke } from "@tauri-apps/api/tauri";
+import DetailListView from './DetailListView';
 import React, { useEffect, useState } from 'react';
+
 
 const ListView = () => {
   const [listArray, setListArray] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedListId, setSelectedListId] = useState(null);
 
   const handleObtainLists = async () => {
     try {
@@ -19,21 +23,38 @@ const ListView = () => {
     handleObtainLists();
   }, []);
 
+  const handleClick = (id) => {
+    console.log("Opening detail page for ", id);
+    setSelectedListId(id);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedListId(null);
+  };
+
 
   return (
     <div className="flex flex-col pt-12 px-4 items-center h-full w-full bg-gray-800 min-h-screen">
       <div className="flex flex-col items-center w-full space-y-5">
-        {listArray.map(item => (<ListItem {...item} />))}
+        {listArray.map(item => (<ListItem key={item.id} {...item} onClick={() => handleClick(item.id)} />))}
       </div>
+      {showModal && (
+        <Modal onClose={handleCloseModal}>
+          <DetailListView listId={selectedListId} />
+        </Modal>
+      )}
     </div>
   )
 };
 
 export default ListView;
 
-const ListItem = ({ id, name }) => {
+const ListItem = ({ id, name, onClick }) => {
   return (
-    <div className={`w-full md:w-7/12 xl:w-9/12 2xl:w-9/12 rounded shadow-lg bg-gray-900 border-solid border-2 border-red-400
+    <div onClick={onClick}
+      className={`w-full md:w-7/12 xl:w-9/12 2xl:w-9/12 rounded shadow-lg bg-gray-900 border-solid border-2 border-red-400
       transition-transform duration-300 hover:scale-105 hover:cursor-pointer`}>
       <div className="py-2">
         <div className="grid grid-cols-12 items-center mb-2 w-full">
@@ -48,3 +69,17 @@ const ListItem = ({ id, name }) => {
       </div>
     </div>)
 }
+
+const Modal = ({ children, onClose }) => {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 w-screen h-screen">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-lg w-11/12 h-11/12 max-w-screen-lg max-h-screen overflow-auto 
+        border-red-400 border-2 border-solid">
+        <button onClick={onClose} className="button-icon font-bold py-2 px-4 rounded-full absolute top-4 right-4">
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
