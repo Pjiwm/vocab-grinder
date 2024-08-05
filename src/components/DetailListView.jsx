94a6942371b72da97d59a17e811dd95c948d8407
Pjiwm <Pim.Munne@gmail.com>
2024-08-05 02:02:06 +0200

    populate table with word entries from list

--- src/components/DetailListView.jsx
+++ src/components/DetailListView.jsx
@@ -20,7 +20,7 @@ const DetailListView = ({ listId }) => {
   return (
     <div className="w-full">
       <table className="table-auto w-full text-left text-white">
-        <thead className="bg-gray-700">
+        <thead className="bg-gray-700 border">
           <tr>
             <th className="px-4 py-2">Word</th>
             <th className="px-4 py-2">Reading</th>
@@ -28,157 +28,14 @@ const DetailListView = ({ listId }) => {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td className="border px-4 py-2">Malcolm Lockyer</td>
-            <td className="border px-4 py-2">1961</td>
-          </tr>
-          <tr className="bg-gray-700">
-            <td className="border px-4 py-2">Witchy Woman</td>
-            <td className="border px-4 py-2">The Eagles</td>
-            <td className="border px-4 py-2">1972</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td className="border px-4 py-2">Malcolm Lockyer</td>
-            <td className="border px-4 py-2">1961</td>
-          </tr>
-          <tr className="bg-gray-700">
-            <td className="border px-4 py-2">Witchy Woman</td>
-            <td className="border px-4 py-2">The Eagles</td>
-            <td className="border px-4 py-2">1972</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td className="border px-4 py-2">Malcolm Lockyer</td>
-            <td className="border px-4 py-2">1961</td>
-          </tr>
-          <tr className="bg-gray-700">
-            <td className="border px-4 py-2">Witchy Woman</td>
-            <td className="border px-4 py-2">The Eagles</td>
-            <td className="border px-4 py-2">1972</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td className="border px-4 py-2">Malcolm Lockyer</td>
-            <td className="border px-4 py-2">1961</td>
-          </tr>
-          <tr className="bg-gray-700">
-            <td className="border px-4 py-2">Witchy Woman</td>
-            <td className="border px-4 py-2">The Eagles</td>
-            <td className="border px-4 py-2">1972</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td className="border px-4 py-2">Malcolm Lockyer</td>
-            <td className="border px-4 py-2">1961</td>
-          </tr>
-          <tr className="bg-gray-700">
-            <td className="border px-4 py-2">Witchy Woman</td>
-            <td className="border px-4 py-2">The Eagles</td>
-            <td className="border px-4 py-2">1972</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td className="border px-4 py-2">Malcolm Lockyer</td>
-            <td className="border px-4 py-2">1961</td>
-          </tr>
-          <tr className="bg-gray-700">
-            <td className="border px-4 py-2">Witchy Woman</td>
-            <td className="border px-4 py-2">The Eagles</td>
-            <td className="border px-4 py-2">1972</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td className="border px-4 py-2">Shining Star</td>
-            <td className="border px-4 py-2">Earth, Wind, and Fire</td>
-            <td className="border px-4 py-2">1975</td>
-          </tr>
+          {wordsArray.map(entry => (
+            <tr className="bg-gray-800">
+              <td className="border px-4 py-2">{entry.word}</td>
+              <td className="border px-4 py-2">{entry.reading}</td>
+              <td className="border px-4 py-2">{entry.translation}</td>
+            </tr>
 
+          ))}
         </tbody>
       </table>
     </div>
